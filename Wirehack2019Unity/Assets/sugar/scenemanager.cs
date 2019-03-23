b549b7c0ddf2306fdc61963be37bdb1bc674737c
Simon <baeyenssimon@gmail.com>
2019-03-23 02:28:42 +0100

    scenetransitionbuttons reg/login/back

---
+++ Wirehack2019Unity/Assets/sugar/scenemanager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scenemanager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RegisterClicked(){
+        SceneManager.LoadScene("Register", LoadSceneMode.Single);
+    }
+
+    public void LoginClicked(){
+        SceneManager.LoadScene("Login", LoadSceneMode.Single);
+    }
+
+    public void BackClicked(){
+        SceneManager.LoadScene("Start", LoadSceneMode.Single);
+        Debug.Log("test");
+    }
+
+}
